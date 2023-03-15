7028c245f5f1ac6930554e55b7bb43620bc98baa
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-15 19:12:47 +0900

    typescriptへリファクタリング2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import footer = require('./footer.css');
+import './footer.css';
 import { Link } from "react-router-dom";
 
 export function Footer(props: string[]){
     return (
-        <div className={footer.position}>
-            <Link to="/" className={footer.text}>{props[0][0]}</Link>
-            <Link to="/about" className={footer.text}>{props[0][1]}</Link>
-            <Link to="/practical" className={footer.text}>{props[0][2]}</Link>
+        <div className={"position"}>
+            <Link to="/" className={"text"}>{props[0][0]}</Link>
+            <Link to="/about" className={"text"}>{props[0][1]}</Link>
+            <Link to="/practical" className={"text"}>{props[0][2]}</Link>
         </div>
     );
 }

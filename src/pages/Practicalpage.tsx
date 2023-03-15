149a8a91e4f08b59869ac26c0477514ff21471d6
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-15 20:01:37 +0900

    修正

--- src/pages/Practicalpage.tsx
+++ src/pages/Practicalpage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Body } from '../components/body';
 import { useForm } from 'react-hook-form';
 import '../App.css';
